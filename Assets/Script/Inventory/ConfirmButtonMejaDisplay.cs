e38747de9b6e34cc84be1224a0827b1fd16f0b91
KKKrackhead <Kelvinbong93@gmail.com>
2023-01-06 18:19:41 +0700

    Revert "test 1"
    This reverts commit d822a6d3b66f18f27f5448aaf47b97004b92dc44.

---
+++ Assets/Script/Inventory/ConfirmButtonMejaDisplay.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfirmButtonMejaDisplay : MonoBehaviour
+{
+    private int nomorMeja;
+    private int itemIndex;
+    private GameObject tampil;
+    private Inventory inventory;
+
+    public void setInventory(Inventory inventory)
+    {
+        this.inventory = inventory;
+    }
+
+    public void ConfirmButton()
+    {
+        string jeneng = GameObject.Find("UI_PlayerInventory").GetComponent<UI_Inventory>().getJeneng(itemIndex);
+
+        nomorMeja = GameObject.Find("Player").GetComponent<PlayerInteraction>().getMeja();
+
+        if (nomorMeja.Equals(1))
+        {
+            tampil = GameObject.Find("MejaAtasKiriDisplay");
+        }
+        if (nomorMeja.Equals(2))
+        {
+            tampil = GameObject.Find("MejaAtasKananDisplay");
+        }
+
+        tampil.GetComponent<TableInventory>().ChangeImage(jeneng, itemIndex);
+
+        inventory.RemoveItem(itemIndex);
+
+        tampil.GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void setitemIndex(int nomor)
+    {
+        itemIndex = nomor;
+    }
+}
