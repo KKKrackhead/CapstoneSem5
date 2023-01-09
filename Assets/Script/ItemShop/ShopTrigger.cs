3d37214edfcec676ffc79157130de7bef4f744ad
KKKrackhead <Kelvinbong93@gmail.com>
2023-01-09 09:38:42 +0700

    shop UI
    pop in & out

---
+++ Assets/Script/ItemShop/ShopTrigger.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopTrigger : MonoBehaviour
+{
+    [SerializeField] GameObject shopcanvas;
+    [SerializeField] GameObject shopui;
+    bool NearShop;
+    public bool ShopIsOpen;
+    public Vector3 temp;
+
+    void Start()
+    {
+        NearShop = false;
+        ShopIsOpen = false;
+        Debug.Log("Player is not near shop");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        NearShop = true;
+        Debug.Log("Player is near the shop");
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        NearShop = false;
+        Debug.Log("PLayer left the shop");
+    }
+
+    public void Update()
+    {
+        if (NearShop && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("haha yes");
+            shopui.transform.position = new Vector3(4590, 8010, 0) * .2f;
+            ShopIsOpen = true;
+        }
+    }
+
+    public void exitshop()
+    {
+        shopui.transform.position = new Vector3(-4590, -8010, 0) * .2f;
+        ShopIsOpen = false;
+    }
+}
